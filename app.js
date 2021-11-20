11a7987db2ad7017f48da7debf4ab1785777ebcc
timjossund <timjossund@gmail.com>
2021-11-20 12:54:47 -0600

    week3

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 const passport = require('passport');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter =  require('./routes/partnerRouter');
-const { isBuffer } = require('util');
-const authenticate = require('./authenticate');
-const config = require('./config');
+const partnerRouter = require('./routes/partnerRouter');
 
-//const url = 'mongodb://localhost:27017/nucampsite';
+const mongoose = require('mongoose');
+
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
+connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
@@ -41,72 +36,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-      const err = new Error('You are not authenticated!');                    
-      err.status = 401;
-      return next(err);
-  } else {
-      return next();
-  }
-}
-
-// function auth(req, res, next) {
-//   console.log(req.session);
-
-//   if (!req.session.user) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         const err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');
-//         err.status = 401;
-//         return next(err);
-//     }
-
-//     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-//     const user = auth[0];
-//     const pass = auth[1];
-//     if (user === 'admin' && pass === 'password') {
-//         req.session.user = 'admin';
-//         return next(); // authorized
-//     } else {
-//         const err = new Error('You are not authenticated!');
-//         res.setHeader('WWW-Authenticate', 'Basic');
-//         err.status = 401;
-//         return next(err);
-//     }
-//   } else {
-//     if (req.session.user === 'admin') {
-//         return next();
-//     } else {
-//         const err = new Error('You are not authenticated!');
-//         err.status = 401;
-//         return next(err);
-//     }
-//   }
-// }
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
